2c4817874397fd80c721d552be3deb871d3f6fae
87right <yuuki.a.181124@gmail.com>
2026-07-01 13:40:28 +0900

    grid displayed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use bevy::prelude::*;
 use std::collections::HashMap;
 
+const CELL_SIZE: f32 = 32.;
+const MAP_HEIGHT: i32 = 16;
+const MAP_WIDTH: i32 = 16;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -23,15 +27,43 @@ pub struct Setup;
 impl Plugin for Setup {
     fn build(&self, app: &mut App) {
         app.insert_resource(WorldGrid::default());
-        app.add_systems(Startup, world_gen);
+        app.add_systems(Startup, (world_gen, setup_camera));
     }
 }
 
+fn setup_camera(mut commands: Commands){
+    commands.spawn((
+        Camera2d,
+        Camera::default(),
+        Transform::from_xyz (
+            MAP_WIDTH  as f32 * CELL_SIZE / 2.,
+            MAP_HEIGHT as f32 * CELL_SIZE / 2.,
+            100.
+        ),
+    ));
+}
+
 fn world_gen(mut world_grid: ResMut<WorldGrid>, mut commands: Commands) {
-    for y in 0..100 {
-        for x in 0..100{
+
+    let empty_color = Color::srgb(0.3, 0.3, 0.3);
+
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
             let v = IVec2 {x, y};
-            let id = commands.spawn((GridPos (v), GridState::Empty)).id();
+            let id = commands.spawn((
+                GridPos (v), 
+                GridState::Empty,
+                Sprite {
+                    color: empty_color,
+                    custom_size: Some(Vec2::new(CELL_SIZE, CELL_SIZE)),
+                    ..default()
+                },
+                Transform::from_xyz (
+                    x as f32 * CELL_SIZE,
+                    y as f32 * CELL_SIZE,
+                    0.0
+                ),
+            )).id();
             world_grid.0.insert(v, id);
         }
     }
